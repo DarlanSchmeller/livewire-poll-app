314eb00216a304bd053286e0fc98bf8dce434bdc
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-11-27 11:10:03 -0300

    feat: Create addOption method to CreatePoll component and integrate it into form

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,9 +7,15 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [];
 
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = $this->title;
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -4,4 +4,14 @@
         <input type="text" name="title" id="title" wire:model.live="title" />
     </form>
     Current title: {{ $title }}
+
+    <div class="mt-4">
+        <button class="btn" wire:click.prevent="addOption">Add Option</button>
+    </div>
+
+    @foreach($options as $index => $option)
+        <div class="mt-4">
+            <p>{{ $index + 1 }}. {{ $option }}</p>
+        </div>
+    @endforeach
 </div>
